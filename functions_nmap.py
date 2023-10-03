2c63bed3e62726215bbef198fdd2563d79ab6191
Henry Weckermann <henry.weckermann@mailbox.org>
2023-10-03 17:37:38 +0200

    removed unused code

--- functions_nmap.py
+++ functions_nmap.py
@@ -8,41 +8,6 @@ from django.conf import settings
 from django.http import HttpResponse
 
 
-# def nmap_scaninfo(request):
-#
-#     tmpfiles = os.listdir('/tmp/')
-#
-#     res = {'out': [], 'scans': {}}
-#
-#     for ff in tmpfiles:
-#         if re.search('\.xml.active$', ff) is not None:
-#             f = ff[0:-7]
-#             res['scans'][f] = {'status': 'active'}
-#             with open('/tmp/' + ff) as n:
-#                 lines = n.readlines()
-#                 for line in lines:
-#                     # res['out'].append(line.strip())
-#                     # <nmaprun scanner="nmap" args="nmap -oG /tmp/test.grep -oX /tmp/scan.xml -sT -sV -sC -T5 scanme.nmap.org" start="1541780258" startstr="Fri Nov  9 16:17:38 2018" version="7.60" xmloutputversion="1.04">
-#
-#                     rx = re.search('args\=.+\-oX \/tmp\/(.+\.xml).+ start\=.+ startstr\=.(.+). version\=', line.strip())
-#                     if rx is not None:
-#                         res['scans'][f]['filename'] = rx.group(1)
-#                         res['scans'][f]['startstr'] = rx.group(2)
-#
-#                     rx = re.search('scaninfo type\=.(.+). protocol\=.(.+). numservices', line.strip())
-#                     if rx is not None:
-#                         res['scans'][f]['type'] = rx.group(1)
-#                         res['scans'][f]['protocol'] = rx.group(2)
-#
-#                     # <finished time="1541780323" timestr="Fri Nov  9 16:18:43 2018" elapsed="65.31" summary="Nmap done at Fri Nov  9 16:18:43 2018; 1 IP address (1 host up) scanned in 65.31 seconds" exit="success"/><hosts up="1" down="0" total="1"/>
-#                     rx = re.search('finished .+ summary\=.(.+). exit\=', line.strip())
-#                     if rx is not None:
-#                         res['scans'][f]['status'] = 'finished'
-#                         res['scans'][f]['summary'] = rx.group(1)
-#
-#     return HttpResponse(json.dumps(res, indent=4), content_type="application/json")
-
-
 def nmap_newscan(request):
     """
     Initiates a new Nmap scan based on parameters from a POST request.
@@ -70,8 +35,6 @@ def nmap_newscan(request):
             nmap_command += 'mv /tmp/' + request.POST['filename'] + '.active /opt/xml/' + request.POST['filename']
             nmap_command += ' &'
 
-            print(nmap_command)
-
             os.popen(nmap_command)
 
             # os.popen('nmap ' + request.POST[
@@ -93,6 +56,5 @@ def nmap_newscan(request):
 
             return HttpResponse(json.dumps(res, indent=4), content_type="application/json")
         else:
-            print("error babeey")
             res = {'error': 'invalid syntax'}
             return HttpResponse(json.dumps(res, indent=4), content_type="application/json")

929501bbf46f8a81b6aca670e319cb49b625916f
Henry Weckermann <henry.weckermann@mailbox.org>
2023-10-18 15:48:29 +0200

    started with implementation

--- functions_ble.py
+++ functions_ble.py
@@ -1,2 +1,24 @@
-from .bluetooth_le.ble_scan import scan_all_devices_and_read_all_fields
+import json
 
+from django.http import HttpResponse
+
+
+def new_ble_scan(request):
+    """
+    Initiates a new BLE scan based on parameters from a POST request.
+
+    Parameters:
+    - request (HttpRequest): The HTTP request object containing POST data.
+
+    Returns:
+    - HttpResponse: A JSON-formatted HTTP response indicating either the POST data or an error message.
+    """
+
+    if request.method == "POST":
+        filename = request.POST.get("zb_filename", "")
+        _ = request.POST.get("zb_interface", "")
+        _ = request.POST.get("zb_channel", "")
+        _ = request.POST.get("zb_pcap_path", "")
+
+    else:
+        return HttpResponse(json.dumps({'error': 'invalid syntax'}, indent=4), content_type="application/json")

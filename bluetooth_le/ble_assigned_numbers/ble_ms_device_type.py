ea28c9899ebc3234d0f515661ba857cd34c0d997
Henry Weckermann <henry.weckermann@mailbox.org>
2023-11-06 15:01:01 +0100

    added official assigned numbers converted to python dicts

---
+++ bluetooth_le/ble_assigned_numbers/ble_ms_device_type.py
@@ -0,0 +1,13 @@
+ms_device_type = {
+    1: "Xbox One",
+    6: "Apple iPhone",
+    7: "Apple iPad",
+    8: "Android device",
+    9: "Windows 10 Desktop",
+    11: "Windows 10 Phone",
+    12: "Linux device",
+    13: "Windows IoT",
+    14: "Surface Hub",
+    15: "Windows laptop",
+    16: "Windows tablet"
+}
---
+++ bluetooth_le/ble_assigned_numbers/ble_ms_flags_and_device_status.py
@@ -0,0 +1,7 @@
+ms_flags_and_device_status = {
+    "00": "None",
+    "01": "Remote Session Hosted",
+    "02": "Remote Session Not Hosted",
+    "04": "Near Share Auth Policy Same User",
+    "08": "Near Share Auth Policy Permissive"
+}

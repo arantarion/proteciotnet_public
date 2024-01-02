56bf8b59fbfab769e8532324711e1243e0c5d58f
Henry Weckermann <henry.weckermann@mailbox.org>
2024-01-02 14:22:56 +0100

    fixed some errors and made more user-friendly

--- run_server.sh
+++ run_server.sh
@@ -1,14 +1,46 @@
 #!/bin/bash
 
-if ifconfig eth0 >/dev/null 2>&1; then
-    ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
-elif ifconfig enx3448edb6e7a8 >/dev/null 2>&1; then
-    ip4=$(/sbin/ip -o -4 addr list enx3448edb6e7a8 | awk '{print $4}' | cut -d/ -f1)
-elif ifconfig eno2 >/dev/null 2>&1; then
-    ip4=$(/sbin/ip -o -4 addr list eno2 | awk '{print $4}' | cut -d/ -f1)
-else
+# This script checks for active network interfaces, retrieves the IP address,
+# activates a Python virtual environment, and runs the ProtecIoTnet server.
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1"
+}
+
+# root check
+if [ "$(id -u)" != "0" ]; then
+    log "Error: Please run as root"
+    exit 1
+fi
+log "Running as root user."
+
+
+get_ip() {
+    local interface=$1
+    ip=$(/sbin/ip -o -4 addr list "$interface" | awk '{print $4}' | cut -d/ -f1)
+    if [[ -n $ip ]]; then
+        echo "$ip"
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+if ! ip4=$(get_ip eth0) && ! ip4=$(get_ip enx3448edb6e7a8) && ! ip4=$(get_ip eno2) && ! ip4=$(get_ip wlo1); then
     ip4="localhost"
+    log "No active network interface found. Defaulting to localhost."
 fi
 
-bash /opt/proteciotnet/proteciotnet_dev/nmap/runcron.sh > /dev/null 2>&1 &
+
+log "Activating Python virtual environment."
+source /opt/proteciotnet/proteciotnet_dev/venv/bin/activate || { log "Error: Failed to activate virtual environment."; exit 1; }
+
+
+#log "Running scripts."
+#bash /opt/proteciotnet/proteciotnet_dev/nmap/runcron.sh > /dev/null 2>&1 &
+#log "Started nmap script in background."
+
 python3 /opt/proteciotnet/manage.py runserver $ip4:8000
+
+

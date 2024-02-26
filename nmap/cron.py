c147a2ecbb26f2dd6af325e7e0474f097cbf0356
Henry Weckermann <henry.weckermann@mailbox.org>
2024-02-26 18:24:17 +0100

    completed documentation for these files

--- nmap/cron.py
+++ nmap/cron.py
@@ -2,65 +2,109 @@ import json
 import os
 import re
 import time
+import logging
 
-cdir = os.path.dirname(os.path.realpath(__file__))
+from configparser import ConfigParser, ExtendedInterpolation
 
-schedfiles = os.listdir(cdir + '/schedule/')
+"""
+The script parses an handels the creation of cronjobs for repeated nmaps scans.
+It is not intended to be run alone. Please use the ProtecIoTnet web-frontend instead.
 
-print(f"cdir {cdir}")
-print(f"schedfile {schedfiles}")
+"""
 
-def gethours(f):
+# cdir = os.path.dirname(os.path.realpath(__file__))
+
+logger = logging.getLogger(__name__)
+
+try:
+    config_cron = ConfigParser(interpolation=ExtendedInterpolation())
+    config_cron.read('../proteciotnet.config')
+
+    _PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY = config_cron.get('WIFI_PATHS', 'proteciotnet_nmap_schedule_directory')
+    _NSE_SCRIPTS_DIRECTORY = config_cron.get('WIFI_PATHS', 'nse_scripts_directory')
+
+    logger.info("Successfully loaded config file 'proteciotnet.config'")
+except Exception as e:
+    logger.error(f"Could not load configuration values from 'proteciotnet.config'. Error: {e}")
+    exit(-3)
+
+schedfiles = os.listdir(_PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY)
+logger.debug(f"Schedule Files: {schedfiles}")
+
+
+def gethours(duration: str) -> int:
+    """
+    Converts a time duration string into its equivalent number of seconds.
+
+    Args:
+    - duration (str): A string representing the time duration. It can be one of the following:
+        - '1h' for 1 hour
+        - '1d' for 1 day
+        - '1w' for 1 week
+        - '1m' for 1 month
+
+    Returns:
+    - int: The number of seconds equivalent to the input time duration.
+
+    Raises:
+    - KeyError: If the input string is not one of the supported time duration strings.
+    """
     return {
         '1h': 3600,
         '1d': 86400,
         '1w': 604800,
         '1m': 2592000
-    }[f]
+    }[duration]
 
 
 for i in schedfiles:
     if re.search('^[a-f0-9]{32,32}\.json$', i.strip()) is not None:
-        sched = json.loads(open(cdir + '/schedule/' + i, "r").read())
+        sched = json.loads(open(f'{_PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY}/{i}', "r").read())
 
-        print(f"sched {sched}")
+        logger.debug(f"sched {sched}")
 
         nextrun = (sched['lastrun'] + gethours(sched['params']['frequency']))
-        print(nextrun)
+        logger.debug(nextrun)
 
         if nextrun <= time.time():
             sched['number'] = (sched['number'] + 1)
-            print("[RUN]   scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(
-                nextrun) + " / now:" + str(time.time()) + ")")
+            logger.debug(
+                f"[RUN] scan:{sched['params']['filename']} id:{sched['number']} (nextrun:{nextrun} / now:{time.time()})")
 
             sched['lastrun'] = time.time()
 
-            print('nmap ' + sched['params']['params'] + ' --script=' + cdir + '/nse/ -oX /tmp/' + str(
+            logger.debug('nmap ' + sched['params']['params'] + ' --script=' + cdir + '/nse/ -oX /tmp/' + str(
                 sched['number']) + '_' + sched['params']['filename'] + '.active ' + sched['params'][
-                                   'target'] + ' > /dev/null 2>&1 && ' +
-                               'sleep 5 && mv /tmp/' + str(sched['number']) + '_' + sched['params'][
-                                   'filename'] + '.active /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' +
-                               sched['params']['filename'] + ' && ' +
-                               'ls -lart /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' + sched['params'][
-                                   'filename'] + ' && python3 ' + cdir + '/cve.py webmapsched_' + str(
+                             'target'] + ' > /dev/null 2>&1 && ' +
+                         'sleep 5 && mv /tmp/' + str(sched['number']) + '_' + sched['params'][
+                             'filename'] + '.active /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' +
+                         sched['params']['filename'] + ' && ' +
+                         'ls -lart /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' + sched['params'][
+                             'filename'] + ' && python3 ' + cdir + '/cve.py webmapsched_' + str(
                 sched['lastrun']) + '_' + sched['params']['filename'] + '')
 
-            nmapout = os.popen('nmap ' + sched['params']['params'] + ' --script=' + cdir + '/nse/ -oX /tmp/' + str(
-                sched['number']) + '_' + sched['params']['filename'] + '.active ' + sched['params'][
-                                   'target'] + ' > /dev/null 2>&1 && ' +
-                               'sleep 5 && mv /tmp/' + str(sched['number']) + '_' + sched['params'][
-                                   'filename'] + '.active /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' +
-                               sched['params']['filename'] + ' && ' +
-                               'ls -lart /opt/xml/webmapsched_' + str(sched['lastrun']) + '_' + sched['params'][
-                                   'filename'] + ' && python3 ' + cdir + '/cve.py webmapsched_' + str(
-                sched['lastrun']) + '_' + sched['params']['filename'] + '').readlines()
+            logger.debug(
+                f"nmap {sched['params']['params']} --script={_NSE_SCRIPTS_DIRECTORY} -oX /tmp/{sched['number']}_{sched['params']['filename']}.active {sched['params']['target']} > /dev/null 2>&1 && " +
+                f"sleep 5 && "
+                f"mv /tmp/{sched['number']}_{sched['params']['filename']}.active /opt/xml/webmapsched_{sched['lastrun']}_{sched['params']['filename']} && " +
+                f"ls -lart /opt/xml/webmapsched_{sched['lastrun']}_{sched['params']['filename']} && " +
+                f"python3 {_PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY}/cve.py webmapsched_{sched['lastrun']}_{sched['params']['filename']}"
+            )
 
-            print(nmapout)
+            nmapout = os.popen(
+                f"nmap {sched['params']['params']} --script={_NSE_SCRIPTS_DIRECTORY} -oX /tmp/{sched['number']}_{sched['params']['filename']}.active {sched['params']['target']} > /dev/null 2>&1 && " +
+                f"sleep 5 && "
+                f"mv /tmp/{sched['number']}_{sched['params']['filename']}.active /opt/xml/webmapsched_{sched['lastrun']}_{sched['params']['filename']} && " +
+                f"ls -lart /opt/xml/webmapsched_{sched['lastrun']}_{sched['params']['filename']} && " +
+                f"python3 {_PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY}/cve.py webmapsched_{sched['lastrun']}_{sched['params']['filename']}"
+                )
 
-            f = open(cdir + '/schedule/' + i, "w")
+            logger.debug(f"nmapout: {nmapout}")
+
+            f = open(f'{_PROTECIOTNET_NMAP_SCHEDULE_DIRECTORY}/{i}', "w")
             f.write(json.dumps(sched, indent=4))
 
             time.sleep(10)
         else:
-            print("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(
+            logger.debug("[SKIP]  scan:" + sched['params']['filename'] + " id:" + str(sched['number']) + " (nextrun:" + str(
                 nextrun) + " / now:" + str(time.time()) + ")")
--- static/py/create_json_vendor_macs.py
+++ static/py/create_json_vendor_macs.py
@@ -1,6 +1,16 @@
 import xml.etree.ElementTree as ET
 import json
 
+"""
+The script parses an XML file containing MAC prefixes and corresponding vendor names,
+and creates a dictionary mapping MAC prefixes to vendor names.
+
+Usage:
+1. Ensure the 'vendorMacs.xml' file is present in the same directory as the script.
+2. Run the script to generate the dictionary mapping.
+3. save this to something useful
+"""
+
 tree = ET.parse('vendorMacs.xml')
 root = tree.getroot()
 
